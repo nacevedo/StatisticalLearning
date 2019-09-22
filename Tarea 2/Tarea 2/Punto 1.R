acb67b8b4b8251982c795e0590c46ff3e486a382
gabyau <gabyaldanau@gmail.com>
2019-09-22 09:16:29 -0500

    Punto 1 final

--- Tarea 2/Tarea 2/Punto 1.R
+++ Tarea 2/Tarea 2/Punto 1.R
@@ -2,12 +2,12 @@ x = c(3, 4.3, 6, 7, 9.1, 10.3)
 N = 6
 y = c(0,1,2,2,4,3)
 
-L = matrix(c(1,0,0,0,0,0,
-             0,1,0,0,0,0,
-             0,0,1,0,0,0,
-             0,0,0,1,0,0,
-             0,0,0,0,1,0,
-             0,0,0,0,0,1), ncol = 6)
+L = matrix(c(0.5,0.5,0,0,0,0,
+             1/3,1/3,1/3,0,0,0,
+             0,1/3,1/3,1/3,0,0,
+             0,0,1/2,1/2,0,0,
+             0,0,0,1/2,1/2,0,
+             0,0,0,0,1/2,1/2), ncol = 6)
 
 
 Lfun=function(h){
@@ -16,15 +16,15 @@ Lfun=function(h){
     cond = vector()
     zz=rep(x[i],times=N)
     for(j in 1:N){
-      cond[j] = ifelse(abs(x[i]-x[j]) > (h+0.01), 0, 1)
+      cond[j] = ifelse(abs(x[i]-x[j]) > (h), 0, 1)
   }
-    bottom=sum((1-(zz-x)^2/h)^2*cond)
-    L[i,]=(1-(zz-x)^2/h)^2*cond/bottom
+    bottom=sum((1-abs(zz-x)/h)*cond)
+    L[i,]=(1-abs(zz-x)/h)*cond/bottom
   }
   return(L)
 }
 
-h=1.2 #You can change the bandwidth
+h=2.8 #You can change the bandwidth
 L = Lfun(h)
 f_hat=c(L%*%y)
 
@@ -37,7 +37,7 @@ MSE=(1/N)*sum(((y-ff)/(1-ll))^2)
 
 
 # GCV
-h = 0.8
+h = 2
   hh=h
   ll=diag(Lfun(hh))
   ff=c(Lfun(hh)%*%y)
