c44a90bd2ab9d6835484c000d0fe20a50056e1f7
gabyau <gabyaldanau@gmail.com>
2019-09-22 17:09:28 -0500

    Punto 4 sin 4.4

--- Tarea 2/Tarea 2/Punto 4.R
+++ Tarea 2/Tarea 2/Punto 4.R
@@ -58,7 +58,21 @@ for(k in 1:length(h)){
     x_train = train[,1]
     y_train = train[,2]
     
-    ff = L(hh,x_test,x_train)%*%y_train
+    N = length(x_test)
+    N2 = length(x_train)
+    mat=matrix(rep(0,times=N*N2), ncol=N2)
+    
+    for(m in 1:N){
+      cond = 0
+      zz=rep(x_test[m],times=N2)
+      for(j in 1:N2){
+        cond[j] = ifelse(abs(x_test[m]-x_train[j]) > hh, 0, 1)
+      }
+      bottom=sum((1-(zz-x_train)^2/hh)^2*cond)
+      mat[m,]=(1-(zz-x_train)^2/hh)^2*cond/bottom
+    }
+    
+    ff = mat%*%y_train
     MSE[i] = mean((ff-y_test)^2)
   }
   MSE_final[k] = mean(MSE)
@@ -73,7 +87,6 @@ min(MSE_final)
 folds <- cut(seq(1,nrow(data)),breaks=10,labels=FALSE)
 
 MSE_final10 = vector()
-h=seq(.1,5,by=.1)
 
 for(k in 1:length(h)){
   MSE = vector()
@@ -87,7 +100,21 @@ for(k in 1:length(h)){
     x_train = train[,1]
     y_train = train[,2]
     
-    ff = L(hh,x_test,x_train)%*%y_train
+    N = length(x_test)
+    N2 = length(x_train)
+    mat=matrix(rep(0,times=N*N2), ncol=N2)
+    
+    for(m in 1:N){
+      cond = 0
+      zz=rep(x_test[m],times=N2)
+      for(j in 1:N2){
+        cond[j] = ifelse(abs(x_test[m]-x_train[j]) > hh, 0, 1)
+      }
+      bottom=sum((1-(zz-x_train)^2/hh)^2*cond)
+      mat[m,]=(1-(zz-x_train)^2/hh)^2*cond/bottom
+    }
+    
+    ff = mat%*%y_train
     MSE[i] = mean((ff-y_test)^2)
   }
   MSE_final10[k] = mean(MSE)
@@ -96,7 +123,99 @@ plot(h, MSE_final10, type = 'l', xlab = 'h', ylab = 'MSE', main = '10 - Fold CV'
 h[which.min(MSE_final10)]
 min(MSE_final10)
 
-par(mfrow =c(1,2))
-plot(h, MSE_final, type = 'l', xlab = 'h', ylab = 'MSE', main = '5 - Fold CV')
-plot(h, MSE_final10, type = 'l', xlab = 'h', ylab = 'MSE', main = '10 - Fold CV')
+# Comparacion 
+plot(h, MSE_final, type = 'l', xlab = 'h', ylab = 'MSE', main = 'Cross Validation', col = 4)
+lines(h, MSE_final10, type = 'l', xlab = 'h', ylab = 'MSE', col = 2)
+legend('topright',legend = c('5 - Fold CV', '10 - Fold CV'),col=c("red", "blue"),lty=1, cex=0.8)
+
+
+#### LOOCV ####
+folds <- cut(seq(1,nrow(data)),breaks=400,labels=FALSE)
+
+MSE_final400 = vector()
+
+for(k in 1:length(h)){
+  MSE = vector()
+  hh = h[k]
+  for(i in 1:400){
+    test_i <- which(folds==i,arr.ind=TRUE)
+    test <- data[test_i, ]
+    x_test = test[1]
+    y_test = test[2]
+    train <- data[-test_i, ]
+    x_train = train[,1]
+    y_train = train[,2]
+    
+    N = length(x_test)
+    N2 = length(x_train)
+    mat=matrix(rep(0,times=N*N2), ncol=N2)
+    
+    for(m in 1:N){
+      cond = 0
+      zz=rep(x_test[m],times=N2)
+      for(j in 1:N2){
+        cond[j] = ifelse(abs(x_test[m]-x_train[j]) > hh, 0, 1)
+      }
+      bottom=sum((1-(zz-x_train)^2/hh)^2*cond)
+      mat[m,]=(1-(zz-x_train)^2/hh)^2*cond/bottom
+    }
+    
+    ff = mat%*%y_train
+    MSE[i] = mean((ff-y_test)^2)
+  }
+  MSE_final400[k] = mean(MSE)
+}
+plot(h, MSE_final400, type = 'l', xlab = 'h', ylab = 'MSE', main = '10 - Fold CV')
+h[which.min(MSE_final400)]
+min(MSE_final400)
+
+
+# Comparacion 
+plot(h, MSE_final, type = 'l', xlab = 'h', ylab = 'MSE', main = 'Cross Validation', col = 4)
+lines(h, MSE_final10, type = 'l', xlab = 'h', ylab = 'MSE', col = 2)
+lines(h, MSE_final400, type = 'l', xlab = 'h', ylab = 'MSE', col = 9)
+
+legend('topright',legend = c('5 - Fold CV', '10 - Fold CV', 'LOOCV'),col=c("red", "blue","black"),lty=1, cex=0.8)
+
+plot(h, MSE_final5, type = 'l', xlab = 'h', ylab = 'MSE', main = '10 - Fold CV')
+h[which.min(MSE_final5)]
+min(MSE_final5)
+
+
+### IC ###
+
+
+
+#### Bootstrap ###
+B = 500
+ybar_boot = c(rep(0,times=B)) # Creacion de vector vacio
+
+
+for(i in 1:B){
+  sample = sample(seq(1:N), N, replace = T)
+  x_train = x[sample]
+  y_train = y[sample]
+  x_test = c(3, 4)
+  
+  NN = length(x_test)
+  N2 = length(x_train)
+  mat=matrix(rep(0,times=NN*N2), ncol=N2)
+  
+  for(m in 1:NN){
+    cond = 0
+    zz=rep(x_test[m],times=N2)
+    for(j in 1:N2){
+      cond[j] = ifelse(abs(x_test[m]-x_train[j]) > hh, 0, 1)
+    }
+    bottom=sum((1-(zz-x_train)^2/hh)^2*cond)
+    mat[m,]=(1-(zz-x_train)^2/hh)^2*cond/bottom
+  }
+  
+  ybar_boot[i] = (mat%*%y)[1]-(mat%*%y)[2]
+}
+
+
+plot(density(ybar_boot))  # Plot de medidas obtenidas con el bootstrap
+c(quantile(ybar_boot,0.025),quantile(ybar_boot,0.975))
+
 
