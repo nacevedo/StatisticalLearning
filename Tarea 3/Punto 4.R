b95166ce73c962824c1a538004602dc455639da0
gabyau <gabyaldanau@gmail.com>
2019-11-05 16:35:21 -0500

    Punto 4 y 5 a medias

--- Tarea 3/Punto 4.R
+++ Tarea 3/Punto 4.R
@@ -1,6 +1,6 @@
 # Punto 4
-
-library(Rfast)
+library(emdbook)
+#library(Rfast)
 # 1 Generar
 #Generacion de inputs y tamano de muestra
 N=500
@@ -60,35 +60,203 @@ AUCnuestro(test$Y,predicted$posterior[,2])
 
 
 #paso 0
-train = sample(1:length(y_train),175)
-x_grupo1 = x_train[-train,]
-y_grupo1 = y_train[-train]
-x_grupo2 = x_train[train,]
-y_grupo2 = y_train[train]
+grupo1 = train[train$Y == 1,]
+grupo0 = train[train$Y == -1,]
 
-#paso 1
+tamano1 = floor(nrow(grupo1)/3)
+tamano0 = floor(nrow(grupo0)/3)
 
-media_x_g1 = colMeans(x_grupo1)
-media_x_g2 = colMeans(x_grupo2)
+grupo11 = grupo1[1:tamano1,]
+grupo12 = grupo1[tamano1+1:2*tamano1,]
+grupo13 = grupo1[2*tamano1+1:nrow(grupo1),]
 
-var_x_g1 = var(x_grupo1)
-var_x_g2 = var(x_grupo2)
+grupo01 = grupo0[1:tamano0,]
+grupo02 = grupo0[tamano0+1:2*tamano0,]
+grupo03 = grupo0[2*tamano0+1:nrow(grupo0),]
 
-#paso 2 
 
-#denominador
+p_g11 = rep(0,nrow(grupo1))
+p_g12 = rep(0,nrow(grupo1))
+p_g13 = rep(0,nrow(grupo1))
 
-p_g1 = rep(0,length(x_grupo1))
-p_g2 = rep(0,length(x_grupo2))
+p_g01 = rep(0,nrow(grupo0))
+p_g02 = rep(0,nrow(grupo0))
+p_g03 = rep(0,nrow(grupo0))
 
-for(i in 1:length(x_train[,1])){
+
+prev_11 = rep(1,length(grupo1))
+prev_12 = rep(1,length(grupo1))
+prev_13 = rep(1,length(grupo1))
+
+prev_01 = rep(1,length(grupo0))
+prev_02 = rep(1,length(grupo0))
+prev_03 = rep(1,length(grupo0))
+
+
+suma = 1
+j = 0
+
+while (suma != 0) {
+  j = j+1
   
-  p_g1[i] = dmvnorm(x_train[i,], mu = media_x_g1, sigma = var_x_g1)
-  p_g2[i] = dmvnorm(x_train[i,], mu = media_x_g2, sigma = var_x_g2)
+  # Medias y varianzas de grupos
+  media_x_g11 = colMeans(grupo11)[1:2]
+  media_x_g12 = colMeans(grupo12)[1:2]
+  media_x_g13 = colMeans(grupo13)[1:2]
+  
+  media_x_g01 = colMeans(grupo01)[1:2]
+  media_x_g02 = colMeans(grupo02)[1:2]
+  media_x_g03 = colMeans(grupo03)[1:2]
+  
+  x_grupo1 = grupo1[,1:2]
+  x_grupo0 = grupo0[,1:2]
+  
+  var_x_g1 = var(x_grupo1)
+  var_x_g0 = var(x_grupo0)
+  
+  
+  # Probabilidades de SubGrupos
+  for(i in 1:nrow(grupo1)){
+    p_g11[i] = dmvnorm(c(grupo1[i,1],grupo1[i,2]), mu = media_x_g11, Sigma = var_x_g1)
+    p_g12[i] = dmvnorm(c(grupo1[i,1],grupo1[i,2]), mu = media_x_g12, Sigma = var_x_g1)
+    p_g13[i] = dmvnorm(c(grupo1[i,1],grupo1[i,2]), mu = media_x_g13, Sigma = var_x_g1)
+  }
+  
+  for(i in 1:nrow(grupo0)){
+    p_g01[i] = dmvnorm(c(grupo0[i,1],grupo0[i,2]), mu = media_x_g01, Sigma = var_x_g0)
+    p_g02[i] = dmvnorm(c(grupo0[i,1],grupo0[i,2]), mu = media_x_g02, Sigma = var_x_g0)
+    p_g03[i] = dmvnorm(c(grupo0[i,1],grupo0[i,2]), mu = media_x_g03, Sigma = var_x_g0)
+    
+  }
+  
+  #paso 3: Cambio de grupo
+  grupo11 = matrix(ncol=3)
+  grupo12 = matrix(ncol=3)
+  grupo13 = matrix(ncol=3)
+  
+  grupo01 = matrix(ncol=3)
+  grupo02 = matrix(ncol=3)
+  grupo03 = matrix(ncol=3)
+  
+  for(i in 1:nrow(grupo1)){
+    if (p_g11[i] > p_g12[i] & p_g11[i] > p_g13[i]){
+      grupo11 = rbind(grupo11,grupo1[i,])
+    } else if (p_g12[i] > p_g11[i] & p_g12[i] > p_g13[i]){
+      grupo12 = rbind(grupo12,grupo1[i,])
+    } else 
+      grupo13 = rbind(grupo13,grupo1[i,])
+  }
+  
+  for(i in 1:nrow(grupo0)){
+    if (p_g01[i] > p_g02[i] & p_g11[i] > p_g13[i]){
+      grupo11 = rbind(grupo11,grupo1[i,])
+    } else if (p_g12[i] > p_g11[i] & p_g12[i] > p_g13[i]){
+      grupo12 = rbind(grupo12,grupo1[i,])
+    } else 
+      grupo13 = rbind(grupo13,grupo1[i,])
+  }
+  
+  
+  for (i in 1:length(train)){
+    if (p_g11[i] > p_g12[i] & p_g11[i] > p_g13[i] & p_g11[i] > p_g21[i] & p_g11[i] > p_g22[i] & p_g11[i] > p_g23[i]){
+      x_grupo11 = rbind(x_grupo11,x_train[i,])
+    } else if (p_g12[i] > p_g11[i] & p_g12[i] > p_g13[i] & p_g12[i] > p_g21[i] & p_g12[i] > p_g22[i] & p_g12[i] > p_g23[i]){
+      x_grupo12 = rbind(x_grupo12,x_train[i,])
+    } else if (p_g13[i] > p_g11[i] & p_g13[i] > p_g12[i] & p_g13[i] > p_g21[i] & p_g13[i] > p_g22[i] & p_g13[i] > p_g23[i]){
+      x_grupo13 = rbind(x_grupo13,x_train[i,])
+    } else if (p_g21[i] > p_g12[i] & p_g21[i] > p_g13[i] & p_g21[i] > p_g11[i] & p_g21[i] > p_g22[i] & p_g21[i] > p_g23[i]){
+      x_grupo21 = rbind(x_grupo21,x_train[i,])
+    } else if (p_g22[i] > p_g11[i] & p_g22[i] > p_g13[i] & p_g22[i] > p_g21[i] & p_g22[i] > p_g12[i] & p_g22[i] > p_g23[i]){
+      x_grupo22 = rbind(x_grupo22,x_train[i,])
+    } else {
+      x_grupo23 = rbind(x_grupo23,x_train[i,])
+    }
+    
+  }
+  
+  x_grupo11 = x_grupo11[2:nrow(x_grupo11),]
+  x_grupo12 = x_grupo12[2:nrow(x_grupo12),]
+  x_grupo13 = x_grupo13[2:nrow(x_grupo13),]
+  
+  x_grupo21 = x_grupo21[2:nrow(x_grupo21),]
+  x_grupo22 = x_grupo22[2:nrow(x_grupo22),]
+  x_grupo23 = x_grupo23[2:nrow(x_grupo23),]
+  
+  
+  # Revisar si cambiaron
+  if (length(x_grupo11) == length(prev_11) && length(x_grupo12) == length(prev_12) && length(x_grupo13) == length(prev_13) && length(x_grupo21) == length(prev_21) && length(x_grupo22) == length(prev_22) && length(x_grupo23) == length(prev_23)){
+    suma = sum(prev_11 != x_grupo11) + sum(prev_12 != x_grupo12) + sum(prev_13 != x_grupo13) + sum(prev_21 != x_grupo21) + sum(prev_22 != x_grupo22) + sum(prev_23 != x_grupo23)
+  } else {
+    suma = 100
+     }
+  
+  # Actualizacion prev
+  prev_11 = x_grupo11
+  prev_12 = x_grupo12
+  prev_13 = x_grupo13
+  
+  prev_21 = x_grupo21
+  prev_22 = x_grupo22
+  prev_23 = x_grupo23
+  
+  print(suma)
+  print(j)
   
 }
 
-#paso 3
+# Test
+p_g11 = rep(0,length(y_test))
+p_g12 = rep(0,length(y_test))
+p_g13 = rep(0,length(y_test))
 
-x_grupo1 = x_grupo1[p_g1 > p_g2]
-x_grupo2 = x_grupo2[p_g1 <= p_g2]
+p_g21 = rep(0,length(y_test))
+p_g22 = rep(0,length(y_test))
+p_g23 = rep(0,length(y_test))
+
+# Probabilidades de Grupos
+for(i in 1:length(y_test)){
+  p_g11[i] = dmvnorm(x_test[i,], mu = media_x_g11, Sigma = var_x_g1)
+  p_g12[i] = dmvnorm(x_test[i,], mu = media_x_g12, Sigma = var_x_g1)
+  p_g13[i] = dmvnorm(x_test[i,], mu = media_x_g13, Sigma = var_x_g1)
+  
+  p_g21[i] = dmvnorm(x_test[i,], mu = media_x_g21, Sigma = var_x_g2)
+  p_g22[i] = dmvnorm(x_test[i,], mu = media_x_g22, Sigma = var_x_g2)
+  p_g23[i] = dmvnorm(x_test[i,], mu = media_x_g23, Sigma = var_x_g2)
+  
+}
+
+#paso 3: Cambio de grupo
+x_grupo11_test = matrix(ncol=2)
+x_grupo12_test = matrix(ncol=2)
+x_grupo13_test = matrix(ncol=2)
+
+x_grupo21_test = matrix(ncol=2)
+x_grupo22_test = matrix(ncol=2)
+x_grupo23_test = matrix(ncol=2)
+
+
+for (i in 1:length(y_test)){
+  if (p_g11[i] > p_g12[i] & p_g11[i] > p_g13[i] & p_g11[i] > p_g21[i] & p_g11[i] > p_g22[i] & p_g11[i] > p_g23[i]){
+    x_grupo11_test = rbind(x_grupo11_test,x_test[i,])
+  } else if (p_g12[i] > p_g11[i] & p_g12[i] > p_g13[i] & p_g12[i] > p_g21[i] & p_g12[i] > p_g22[i] & p_g12[i] > p_g23[i]){
+    x_grupo12_test = rbind(x_grupo12_test,x_test[i,])
+  } else if (p_g13[i] > p_g11[i] & p_g13[i] > p_g12[i] & p_g13[i] > p_g21[i] & p_g13[i] > p_g22[i] & p_g13[i] > p_g23[i]){
+    x_grupo13_test = rbind(x_grupo13_test,x_test[i,])
+  } else if (p_g21[i] > p_g12[i] & p_g21[i] > p_g13[i] & p_g21[i] > p_g11[i] & p_g21[i] > p_g22[i] & p_g21[i] > p_g23[i]){
+    x_grupo21_test = rbind(x_grupo21_test,x_test[i,])
+  } else if (p_g22[i] > p_g11[i] & p_g22[i] > p_g13[i] & p_g22[i] > p_g21[i] & p_g22[i] > p_g12[i] & p_g22[i] > p_g23[i]){
+    x_grupo22_test = rbind(x_grupo22_test,x_test[i,])
+  } else {
+    x_grupo23_test = rbind(x_grupo23_test,x_test[i,])
+  }
+  
+}
+
+x_grupo11_test = x_grupo11_test[2:nrow(x_grupo11_test),]
+x_grupo12_test = x_grupo12_test[2:nrow(x_grupo12_test),]
+x_grupo13_test = x_grupo13_test[2:nrow(x_grupo13_test),]
+
+x_grupo21_test = x_grupo21_test[2:nrow(x_grupo21_test),]
+x_grupo22_test = x_grupo22_test[2:nrow(x_grupo22_test),]
+x_grupo23_test = x_grupo23_test[2:nrow(x_grupo23_test),]
+
--- Tarea 3/Punto 5.R
+++ Tarea 3/Punto 5.R
@@ -33,6 +33,79 @@ test = as.data.frame(cbind(x_test, y_test))
 colnames(test) = c('X1','X2', 'Y')
 
 # Rotacion
+library(randomForest)
+y_train = as.factor(y_train)
+y_test = as.factor(y_test)
+
 hh=function(theta){
   mm=matrix(c(cos(theta),-sin(theta),sin(theta),cos(theta)),ncol=2)
   return(mm)}
+
+N = 500
+theta = runif(N, 0, 2*pi)
+y = matrix(0,nrow(x_test), N)
+auc = matrix(0,nrow(x_test), N)
+
+for (i in 1:N){
+ rotados = x_train%*%hh(theta[i])
+ bag = randomForest(y_train~., data = rotados, mtry = (dim(rotados)[2]))
+ 
+ imp = importance(bag)
+ #imp
+ #varImpPlot(bag)
+ 
+ rotados_test = x_test%*%hh(theta[i])
+ 
+ test = as.data.frame(cbind(y_test,rotados_test))
+ colnames(test) = c('y_test', 'V1', 'V2')
+ 
+ pred_bag = predict(bag, test, type="prob")
+ y[,i] = predict(bag, test, type = "class")
+ 
+ 
+ library(pROC)
+ 
+ roc_bag=roc(test$y_test,pred_bag[,2])
+ plot(roc_bag)
+ auc[i]=auc(roc_bag)
+} 
+
+ygorro = (y==2)*1+(y==1)*-1
+ybarra = rowMeans(ygorro)
+yfinal = (ybarra<0)*-1+(ybarra>=0)*1
+
+aucfinal = mean(auc)
+
+
+
+
+  ### PREGUNTAR: QUE SE HACE CON LOS DATOS DE CADA ITERACION
+
+test_norot = as.data.frame(cbind(y_test,x_test))
+
+# Random Forest
+rf.data=randomForest(y_train~., data = x_train) # m = 8
+
+imp_rf=importance(rf.data)
+imp_rf
+varImpPlot(rf.data)
+
+pred_rf=predict(rf.data,test_norot,type="prob")
+
+roc_rf=roc(test_norot$y_test,pred_rf[,2])
+plot(roc_rf)
+auc_rf=auc(roc_rf)
+auc_rf
+
+
+# MDA
+library(mda)
+mda_out <- mda(Y~., data = train)
+colnames(test_norot) = c('Y', 'X1', 'X2')
+
+pred_mda=predict(mda_out,test_norot,type="posterior")
+
+roc_mda=roc(test_norot$Y,pred_mda[,2])
+plot(roc_mda)
+auc_mda=auc(roc_mda)
+auc_mda
