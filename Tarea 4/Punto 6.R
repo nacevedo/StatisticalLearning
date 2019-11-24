c9692346df5e23d07429fa67d0cbf261eb9bb423
gabyau <gabyaldanau@gmail.com>
2019-11-24 15:32:16 -0500

    Punto 6.1, sin funcionar

--- Tarea 4/Punto 6.R
+++ Tarea 4/Punto 6.R
@@ -5,3 +5,13 @@ CTG <- read_excel("CTG.xls", sheet = "Raw Data")
 data = CTG[-1,]
 library(tidyverse)
 data = data %>% select(LB,AC,FM,UC,DL,DS,DP,ASTV,MSTV,ALTV,MLTV,Width,Min,Max,Nmax,Nzeros,Mode,Mean,Median,Variance,Tendency)
+
+# 1.Datos atipicos mvoutlier -------
+library(mvoutlier)
+colorData <- color.plot(data, quan=1/2, alpha=0.025)
+chisq.plot(data) # QQ Plot. Quitar outliers manualmente
+uni.plot(data) # Ver outliers como se comportan en cada variable
+# Lo hace sacando la distancia mahanalobis
+# Va a poner c?digo con labels
+
+# 2.Datos atipicos LOF -------
