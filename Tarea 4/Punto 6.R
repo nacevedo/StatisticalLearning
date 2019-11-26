c2f31cacc22684971b87f9484baa2ac34c5f4c8f
gabyau <gabyaldanau@gmail.com>
2019-11-26 17:45:26 -0500

    Punto 6 completo

--- Tarea 4/Punto 6.R
+++ Tarea 4/Punto 6.R
@@ -4,14 +4,48 @@ CTG <- read_excel("CTG.xls", sheet = "Raw Data")
 
 data = CTG[-1,]
 library(tidyverse)
-data = data %>% select(LB,AC,FM,UC,DL,DS,DP,ASTV,MSTV,ALTV,MLTV,Width,Min,Max,Nmax,Nzeros,Mode,Mean,Median,Variance,Tendency)
+data = data %>% select(LB,AC,FM,UC,ASTV,MSTV,ALTV)
+
 
 # 1.Datos atipicos mvoutlier -------
 library(mvoutlier)
-colorData <- color.plot(data, quan=1/2, alpha=0.025)
-chisq.plot(data) # QQ Plot. Quitar outliers manualmente
-uni.plot(data) # Ver outliers como se comportan en cada variable
+data$FM = NULL
+data$ALTV = NULL
+## ESTÁ BIEN QUITAR ESTOS???
+
+
+uni.plot(as.matrix(data), na.omit = TRUE) # Ver outliers como se comportan en cada variable
 # Lo hace sacando la distancia mahanalobis
 # Va a poner c?digo con labels
 
+graph = uni.plot(as.matrix(data), symb = TRUE)
+# Variables: AC, UC, MSTV
+
+
+
 # 2.Datos atipicos LOF -------
+library(DMwR)
+data = CTG[-1,]
+library(tidyverse)
+data = data %>% select(LB,AC,FM,UC,ASTV,MSTV,ALTV)
+
+outlier.scores <- lofactor(data, k=5) ## CUÁNTOS VECINOS USAR??
+plot(density(outlier.scores))
+
+# pick top 5 as outliers
+outliers <- order(outlier.scores, decreasing=T)[1:5]
+labels <- 1:n
+labels[-outliers] <- "."
+biplot(prcomp(data), cex=.8, xlabs=labels)
+# who are outliers
+print(outliers)
+print(data[outliers,])
+
+n <- nrow(data)
+pch <- rep(".", n)
+pch[outliers] <- "+"
+col <- rep("black", n)
+col[outliers] <- "red"
+pairs(data, pch=pch, col=col)
+
+# Variable principal es FM
