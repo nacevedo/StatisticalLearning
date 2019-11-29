64630041b96efbab8e2d6b830991f7f87c7fe1b4
Bibiana Gamba <b.gamba10@uniandes.edu.co>
2019-11-29 08:10:06 -0500

    Algunos cambios al punto 1

--- Tarea 4/Punto 1.R
+++ Tarea 4/Punto 1.R
@@ -1,16 +1,27 @@
 # Punto 1
 library(FNN)
 
-neighbor_boosting = function(lambda, B, k, datos, y){
+neighbor_boosting = function(lambda, B, pk, datos, y){
   
+  datos = train_real
   weights = rep(0,nrow(datos))
   pred = rep(0,nrow(datos))
+  
   for (b in 1:B){
-    train_index = sample(nrow(datos),0.75*nrow(datos),replace = FALSE)
-    train = datos[train_index,]
+    
+    train_index = sample.split(datos[,1], SplitRatio = .75)
+    train = subset(datos, train_index == TRUE)
+    weights_temp = subset(weights, train_index == TRUE)
+    
     neighbors = get.knn(train, k = 5)
-    for (i in 1:nrow(datos)){
-      
+    
+    for (i in 1:nrow(weights)){
+      if (train_index[i]){
+        weights_temp[i] = formula
+      }
+      else{
+        
+      }
     }
       
       
@@ -19,6 +30,12 @@ neighbor_boosting = function(lambda, B, k, datos, y){
 
 ### Datos Default
 library(ISLR)
-  
+library(caTools)
+
 datos = Default
 datos$student = as.numeric(datos$student)
+datos$default = as.numeric(datos$default)
+
+sample = sample.split(datos[,1], SplitRatio = .75)
+train_real = subset(datos, sample == TRUE)
+test_real  = subset(datos, sample == FALSE)
