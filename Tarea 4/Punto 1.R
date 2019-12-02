6ff41f6c6e93942c45daa4f0aed9d4d8a87ea75f
Bibiana Gamba <b.gamba10@uniandes.edu.co>
2019-12-02 11:47:15 -0500

    Punto 1 y otros cambios

--- Tarea 4/Punto 1.R
+++ Tarea 4/Punto 1.R
@@ -8,6 +8,7 @@ neighbor_boosting = function(lambda, B, pk, datos){
   datos = train_real
   weights = rep(0.5,nrow(datos))
   pred = rep(0,nrow(datos))
+  weights_temp = rep(0.5,nrow(datos))
   
   weights_before = weights
   
@@ -15,7 +16,7 @@ neighbor_boosting = function(lambda, B, pk, datos){
     
     train_index = sample.split(datos[,1], SplitRatio = .75)
     train = subset(datos, train_index == TRUE)
-    weights_temp = weights #Hay que arreglar este para que tenga la dimensión de los datos
+    weights_temp = weights
     
     neighbors = get.knn(train[,-1], k = pk)
     cuantos = 0
@@ -23,10 +24,14 @@ neighbor_boosting = function(lambda, B, pk, datos){
       if (train_index[i]){
         cuantos = cuantos + 1
         indices_vecinos = neighbors$nn.index[cuantos,]
-        y = train$default[indices_vecinos]
+        y = datos$default[indices_vecinos]
         w = weights_before[indices_vecinos]
+       
         prediction = crossprod(y,w)/sum(w)
-        weights_temp[i] = (train$default[i] - prediction)^2 #Esta predicción debe ser la de la iteración anterior, puede ser guardar los pesos de la iteración pasada
+        
+        pred[i] = prediction
+        weights_temp[i] = (datos$default[i] - prediction)^2 #Esta predicción debe ser la de la iteración anterior, puede ser guardar los pesos de la iteración pasada
+        
         weights[i] = weights[i] + lambda*(weights_temp[i] - weights[i])
       }
       else{
@@ -37,7 +42,7 @@ neighbor_boosting = function(lambda, B, pk, datos){
     
     weights_before = weights
   }
-  return (weights) 
+  return(cbind(weights, pred))
 }
 
 ### Datos Default
@@ -53,19 +58,113 @@ sample = sample.split(datos[,1], SplitRatio = .75)
 train_real = subset(datos, sample == TRUE)
 test_real  = subset(datos, sample == FALSE)
 
-wei = neighbor_boosting(0.1,300,3,train_real)
+auc_comparar = vector()
 
-neigh = get.knn(test_real[,-1], k = 2)
+for(j in 1:60){
+  respuesta = neighbor_boosting(0.1,50,j,train_real)
+  
+  neigh = get.knnx(train_real[,-1], test_real[,-1], k = j)
+  
+  indices_vecinos = neigh$nn.index
+  y_test = test_real$default
+  y_train = train_real$default
+  
+  w = respuesta[,1]
+  
+  prediction_real = vector()
+  
+  for(i in 1:nrow(test_real)){
+    prediction_real[i] = crossprod(y_train[indices_vecinos[i,]],w[indices_vecinos[i,]])/sum(w[indices_vecinos[i,]]) #!!!
+  }
+  
+  prediction_real
+
+  auc_comparar[j] = auc(y_test, prediction_real)
+  
+}
+
+plot(auc_comparar, type = 'l', xlab = 'Vecinos', ylab = 'AUC', main = 'AUC al variar los vecinos')
+points(auc_comparar)
+k_calibrado = which.max(auc_comparar)
+k_calibrado
+
+
+auc_comparar_lambda = vector()
+
+l = seq(0.001,1,0.005)
+
+for(j in 1:length(l)){
+  
+  respuesta = neighbor_boosting(l[j],50,k_calibrado,train_real)
+  
+  neigh = get.knnx(train_real[,-1], test_real[,-1], k = k_calibrado)
+  
+  indices_vecinos = neigh$nn.index
+  y_test = test_real$default
+  y_train = train_real$default
+  
+  w = respuesta[,1]
+  
+  prediction_real = vector()
+  
+  for(i in 1:nrow(test_real)){
+    prediction_real[i] = crossprod(y_train[indices_vecinos[i,]],w[indices_vecinos[i,]])/sum(w[indices_vecinos[i,]]) #!!!
+  }
+  
+  auc_comparar_lambda[j] = auc(y_test, prediction_real)
+  
+}
+
+plot(l,auc_comparar_lambda, type = 'l', xlab = 'Lambda', ylab = 'AUC', main = 'AUC al variar lambda')
+points(l, auc_comparar_lambda)
+lambda_calibrado = l[which.max(auc_comparar_lambda)]
+lambda_calibrado
+
+auc_comparar_B = vector()
+
+for(j in 1:200){
+  
+  respuesta = neighbor_boosting(lambda_calibrado,j,k_calibrado,train_real)
+  
+  neigh = get.knnx(train_real[,-1], test_real[,-1], k = k_calibrado)
+  
+  indices_vecinos = neigh$nn.index
+  y_test = test_real$default
+  y_train = train_real$default
+  
+  w = respuesta[,1]
+  
+  prediction_real = vector()
+  
+  for(i in 1:nrow(test_real)){
+    prediction_real[i] = crossprod(y_train[indices_vecinos[i,]],w[indices_vecinos[i,]])/sum(w[indices_vecinos[i,]]) #!!!
+  }
+  
+  auc_comparar_B[j] = auc(y_test, prediction_real)
+  
+}
+
+plot(auc_comparar_B, type = 'l', xlab = 'Iteraciones', ylab = 'AUC', main = 'AUC al variar las iteraciones del boosting')
+points(auc_comparar_B)
+B_calibrado = which.max(auc_comparar_B)
+B_calibrado
+
+###### real
+
+respuesta = neighbor_boosting(lambda_calibrado,B_calibrado,k_calibrado,train_real)
+
+neigh = get.knnx(train_real[,-1], test_real[,-1], k = k_calibrado)
 
 indices_vecinos = neigh$nn.index
 y_test = test_real$default
 y_train = train_real$default
-w = wei[indices_vecinos]
+
+w = respuesta[,1]
 
 prediction_real = vector()
 
 for(i in 1:nrow(test_real)){
-  prediction_real[i] = crossprod(y_train[indices_vecinos[i]],w[i])/sum(w[i])
+  prediction_real[i] = crossprod(y_train[indices_vecinos[i,]],w[indices_vecinos[i,]])/sum(w[indices_vecinos[i,]]) #!!!
 }
 
 prediction_real
@@ -73,10 +172,7 @@ prediction_real
 sum(prediction_real != y_test)/length(y_test)
 auc(y_test, prediction_real)
 
-sum(prediction_real == 0)
-sum(prediction_real == 1)
-
-r = roc(prediction_real, y_test)
+r = roc(y_test, prediction_real)
 plot(r, main = 'ROC Boosting')
 a = auc(r)
 a
@@ -91,9 +187,6 @@ imp
 varImpPlot(rand_forest, main = "Importancia de variables en Random Forest")
 
 pred_rf = predict(rand_forest, test_real[,-1])
-
-pred_rf[pred_rf > 0.5] = 1
-pred_rf[pred_rf <= 0.5] = 0
 
 auc(test_real$default, pred_rf)
 
